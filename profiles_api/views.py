4542899367c74451a97d22415792457e4fe000f8
AllenLacwin <allenyassan@gmail.com>
2023-10-07 21:36:48 +0800

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -108,6 +108,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
+
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
